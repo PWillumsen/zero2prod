2491bd1908547567e27deddd05c977647c9af927
PeterWillumsen <pewillumsen@gmail.com>
2023-03-26 00:50:12 +0100

    changed to PgPool, fixed tests

--- src/startup.rs
+++ src/startup.rs
@@ -1,15 +1,17 @@
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, HttpServer, App, web};
+use actix_web::{dev::Server, web, App, HttpServer};
+use sqlx::PgPool;
 
-use crate::routes::{subscribe, health_check};
+use crate::routes::{health_check, subscribe};
 
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgPool) -> Result<Server, std::io::Error> {
+    let pool = web::Data::new(connection);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(pool.clone())
     })
     .listen(listener)?
     .run();
