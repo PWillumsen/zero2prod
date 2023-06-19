743e2861f4a95bd96d22bf58c2b520d2bd650d77
PWillumsen <pewillumsen@gmail.com>
2023-06-19 23:56:10 +0200

    removed sudo from script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,16 +2,16 @@
 set -x
 set -eo pipefail
 if ! [ -x "$(command -v psql)" ]; then
-  echo >&2 "Error: psql is not installed."
-  exit 1
+	echo >&2 "Error: psql is not installed."
+	exit 1
 fi
 if ! [ -x "$(command -v sqlx)" ]; then
-  echo >&2 "Error: sqlx is not installed."
-  echo >&2 "Use:"
-  echo >&2 "    cargo install --version='~0.6' sqlx-cli \
+	echo >&2 "Error: sqlx is not installed."
+	echo >&2 "Use:"
+	echo >&2 "    cargo install --version='~0.6' sqlx-cli \
 --no-default-features --features rustls,postgres"
-  echo >&2 "to install it."
-  exit 1
+	echo >&2 "to install it."
+	exit 1
 fi
 DB_USER="${POSTGRES_USER:=postgres}"
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
@@ -20,15 +20,14 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
 # Allow to skip Docker if a dockerized Postgres database is already running
-if [[ -z "${SKIP_DOCKER}" ]]
-then
-  sudo docker run \
-      -e POSTGRES_USER=${DB_USER} \
-      -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-      -e POSTGRES_DB=${DB_NAME} \
-      -p "${DB_PORT}":5432 \
-      -d postgres \
-      postgres -N 1000
+if [[ -z "${SKIP_DOCKER}" ]]; then
+	docker run \
+		-e POSTGRES_USER=${DB_USER} \
+		-e POSTGRES_PASSWORD=${DB_PASSWORD} \
+		-e POSTGRES_DB=${DB_NAME} \
+		-p "${DB_PORT}":5432 \
+		-d postgres \
+		postgres -N 1000
 fi
 export PGPASSWORD="${DB_PASSWORD}"
 
@@ -36,12 +35,11 @@ export PGPASSWORD="${DB_PASSWORD}"
 #   >&2 echo "Postgres is still unavailable - sleeping"
 # sleep 1 done
 
-
->&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
+echo >&2 "Postgres is up and running on port ${DB_PORT} - running migrations now!"
 
 DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 export DATABASE_URL
 
 sqlx database create
 sqlx migrate run
->&2 echo "Postgres has been migrated, ready to go!"
+echo >&2 "Postgres has been migrated, ready to go!"
