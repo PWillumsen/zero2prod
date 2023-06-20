239875727b0126369c526a5fa5a5c7f09869cfe6
PWillumsen <pewillumsen@gmail.com>
2023-06-20 18:34:22 +0200

    added tracing-actix-web. chapter 4 done

--- src/startup.rs
+++ src/startup.rs
@@ -1,16 +1,15 @@
 use std::net::TcpListener;
 
-use actix_web::middleware::Logger;
+use crate::routes::{health_check, subscribe};
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
-
-use crate::routes::{health_check, subscribe};
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, connection: PgPool) -> Result<Server, std::io::Error> {
     let pool = web::Data::new(connection);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(pool.clone())
