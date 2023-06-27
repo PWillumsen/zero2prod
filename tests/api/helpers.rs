34917e8e39bf06993449331530db425510f86929
PWillumsen <pewillumsen@gmail.com>
2023-06-27 22:29:58 +0200

    refactored tests

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -24,6 +24,19 @@ pub struct TestApp {
     pub db_pool: PgPool,
 }
 
+impl TestApp {
+    pub async fn post_subscriptions(&self, body: String) -> reqwest::Response {
+        let client = reqwest::Client::new();
+        client
+            .post(&format!("{}/subscriptions", &self.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+}
+
 pub async fn spawn_app() -> TestApp {
     Lazy::force(&TRACING);
 
