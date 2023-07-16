bd621a41f633a9e5113258323691159c1507d07b
PWillumsen <pewillumsen@gmail.com>
2023-07-16 15:13:47 +0200

    removed mut

--- src/configuration.rs
+++ src/configuration.rs
@@ -128,7 +128,7 @@ impl DatabaseSettings {
     }
 
     pub fn with_db(&self) -> PgConnectOptions {
-        let mut options = self.without_db().database(&self.database_name);
+        let options = self.without_db().database(&self.database_name);
         options.log_statements(tracing::log::LevelFilter::Trace)
     }
 }
